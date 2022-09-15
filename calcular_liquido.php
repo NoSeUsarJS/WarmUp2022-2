16d9cfe71532e0744b68f3450774fbcc2f98a577
mateogon <mateogon97@hotmail.com>
2022-09-15 18:28:23 -0300

    comisiones agregadas.

--- calcular_liquido.php
+++ calcular_liquido.php
@@ -3,8 +3,34 @@ date_default_timezone_set('America/Santiago');
 
 
 if ( isset($_GET['afp']) and isset($_GET['sueldo']) ){
-	echo "afp: " . $_GET['afp'] . " sueldo:" . $_GET['sueldo'];
-} else {
-  echo "variables not set";
+	$afp = $_GET['afp'];
+	$comision = 0;
+	$sueldo = $_GET['sueldo'];
+	switch ($afp) {
+    case 'AFP Capital':
+        $comision = 1.44;
+        break;
+    case 'AFP Cuprum':
+        $comision = 1.44;
+        break;
+	case 'AFP Habitat':
+        $comision = 1.27;
+        break;
+	case 'AFP Modelo':
+        $comision = 0.58;
+        break;
+	case 'AFP Planvital':
+        $comision = 1.16;
+        break;
+	case 'AFP Provida':
+        $comision = 1.45;
+        break;
+	case 'AFP Uno':
+        $comision = 0.69;
+        break;
+	}
+	echo "afp: " . $afp;
+	echo "comision: " . $comision;
+	echo "sueldo: " . $sueldo;
 }
 ?>
